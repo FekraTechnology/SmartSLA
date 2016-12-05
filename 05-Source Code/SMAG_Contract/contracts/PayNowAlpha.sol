215b2aadd2286080ca618f1a80e265515b283d6f
amgadabdelhafez <amgad@yahoo.com>
2016-12-04 20:43:37 -0500

    deposit caluculations

--- 05-Source Code/SMAG_Contract/contracts/PayNowAlpha.sol
+++ 05-Source Code/SMAG_Contract/contracts/PayNowAlpha.sol
@@ -3,68 +3,75 @@ import "HelperLib.sol";
 
 contract PayNowAlpha {
 		//Task Record Data structure
-		struct TaskRecord {
+		struct taskRecord {
 			address requester;//requester address (mapped to user sys_id in servicenow)
 			address fulfiller;//fulfiller address (mapped to user sys_id in servicenow)
-			bytes32 priority;//priority enum
-			bytes32 currentState;//current state enum
-			bytes32 stateUpdatedOn;//state last update timestamp
-			bytes32 createdOn;//creation timestamp
-			bytes32 incidetUpdatedOn;//last update timestamp
+			uint priority;//priority enum
+			string currentState;//current state enum
+			string stateUpdatedOn;//state last update timestamp
+			string createdOn;//creation timestamp
+			string taskUpdatedOn;//last update timestamp
+			uint deposit;//need an array of structures to hold deposits?
 
-			previousStates[];//array of states mapped to timestamps
+			string[] previousStates;//array of states changes
 		}
 
-	mapping (bytes32 => TaskRecord) public tasks;
+		struct taskState{
+		}
+
+	mapping (string => taskRecord) public tasks;
 
 	uint public numOfIncs;
 
 	//create a new task
-	function createTask(address _requester, bytes32 _sys_id, bytes32 _created) returns(bool success){
-		tasks[_sys_id].requester = _requester;
-		tasks[_sys_id].incidetCreatedOn = _created;
-		tasks[_sys_id].currentState = "open";
+	//this will come for a taskDecomposition module, where a single request creates multiple tasks
+	function createTask(address requester, string sys_id, string createdOn, uint priority) constant returns(bool success){
+		tasks[sys_id].requester = requester;
+		tasks[sys_id].createdOn = createdOn;
+		tasks[sys_id].currentState = "open";
+		tasks[sys_id].deposit = calculateTaskDeposit(priority);
+
 		numOfIncs += 1;
 
 		return true;
 	}
 
-	//create a new task
+	//calculate task deposit based on: priority
 	//TODO: lookup agreement costing model to determine task deposit based on priority
 	//TODO: what else do we need to calculate the deposit?
-	function calculateTaskDeposit(bytes32 priority) returns(bytes31 deposit){
-		if(priority="High") {return "9"};
-		else if(priority="Mideum") {return "8"};
-		else if (priority="Low") {return "7"};
-		return true;
+	function calculateTaskDeposit(uint priority) constant returns(uint depositAmount){
+		if (priority == 1) return 9;
+		else if (priority == 2) return 8;
+		else if (priority == 3) return 7;
+		else return 0;
 	}
 
-	//update an open task
-	function updateTaskState(bytes32 _sys_id, string _newState) returns(bool success){
-		Tasks[_sys_id].stateChangesCounter += 1;
-		Tasks[_sys_id].previousStates[Tasks[_sys_id].stateChangesCounter] = Tasks[_sys_id].currentState;
-		Tasks[_sys_id].currentState = _newState;
-		Tasks[_sys_id].stateUpdatedOn = now;
+	//update the state of an open task
+	function updateTaskState(string sys_id, string newState) constant returns(bool success){
+		tasks[sys_id].previousStates.push(tasks[sys_id].currentState);
+		tasks[sys_id].currentState = newState;
+		tasks[sys_id].stateUpdatedOn = "11:11:11";//now;
 		return true;
+		//TODO need to couple state canges with timestamps, mapping?
 	}
 
 	//read number of open tasks
-	function readOpenTasks() constant returns(uint) {
+	function readOpentasks() constant returns(uint) {
 		return numOfIncs;
 	}
 
 	//read an open task state
-	function readTaskState(bytes32 _sys_id) constant returns(string) {
-		return Tasks[_sys_id].currentState;
+	function readTaskState(string _sys_id) constant returns(string) {
+		return tasks[_sys_id].currentState;
 	}
 
 	//read an open task timestamp
-	function readTaskCreatedOn(bytes32 _sys_id) constant returns(bytes32) {
-		return Tasks[_sys_id].incidetCreatedOn;
+	function readTaskCreatedOn(string _sys_id) constant returns(string) {
+		return tasks[_sys_id].createdOn;
 	}
 	//read an open task timestamp
-	function readIncUpdatedOn(bytes32 _sys_id) constant returns(uint) {
-		return Tasks[_sys_id].stateUpdatedOn;
+	function readIncUpdatedOn(string _sys_id) constant returns(string) {
+		return tasks[_sys_id].stateUpdatedOn;
 	}
 
 }
