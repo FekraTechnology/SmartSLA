0db84f18a5c5b4316c11228b9e158f92beb5ab33
amgadabdelhafez <amgad@yahoo.com>
2016-12-17 12:53:46 -0500

    testingtokensminting

---
+++ 05-Source Code/truffle_contracts/contracts/ITCash.sol
@@ -0,0 +1,98 @@
+/*
+
+This contract os based on an implementation of the Ethereum Token Contracts standard
+Reference spec at https://github.com/ConsenSys/Tokens
+*/
+
+pragma solidity ^0.4.2;
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+contract ITCash {
+    /* Initial MVP constants of ITCash token, these will be public variables */
+    string public standard = 'ITCVersion 0.1';
+    string public name = "ITCash";
+    string public symbol = "ITC";
+    uint8 public decimals = 2;
+    uint256 public totalSupply = 1000;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates an event on the private blockchain that will notify listeners */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    /*
+    Initially this is used in budgetting, initial supply will equal planned demand, later it will be
+    part of setting up a new agreement between provider and consumer, the intial supply equals the
+    total amount of the agreement between the two parties
+    */
+
+    function ITCash(){}
+/* So let's start this function without a constructor for now.
+
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes, reference the agreement with supplier
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+      }
+*/
+
+    //this is part of the budgeting process, each department, group, and user gets an initial funding for
+    function mintITC(uint256 initialSupply){
+      totalSupply = initialSupply;
+
+      balanceOf[msg.sender] = totalSupply;  // Give the creator all initial tokens
+    }
+
+    /* Transfer ITCash, will have an invoice as input, invoice will be processed based on a usage record */
+
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /* Approve and then comunicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
